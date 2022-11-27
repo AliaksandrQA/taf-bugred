945db058e4a58234debcf3f0966059d6abe22578
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-11-27 19:27:16 +0100

    Add singletone class

---
+++ src/main/java/by/academy/driver/DriverSingleton.java
@@ -0,0 +1,43 @@
+package by.academy.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+import static io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver;
+
+public class DriverSingleton {
+    private static WebDriver driver;
+
+    private DriverSingleton() {
+    }
+
+    public static WebDriver getDriver(){
+        System.setProperty("browser", "chrome"); // без этого иногда не запускается
+        if (null == driver){
+            switch (System.getProperty("browser")){
+                case "firefox": {
+                    firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                default: {
+                    chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
+        return driver;
+    }
+
+    public static void closeDriver(){
+        driver.quit();
+        driver = null;
+    }
+}
---
+++ src/main/java/by/academy/ui/BasePage.java
@@ -0,0 +1,4 @@
+package by.academy.ui;
+
+public class BasePage {
+}
